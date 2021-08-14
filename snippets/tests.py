bcb40ba3ea9d820ffea7a8234a0f15bb0e85ea14
HirokiNakazawa <sp.hn_hc4@i.softbank.jp>
2021-08-14 22:06:57 +0900

    1.5

--- snippets/tests.py
+++ snippets/tests.py
@@ -1,3 +1,15 @@
+from django.http import HttpRequest
 from django.test import TestCase
+from snippets.views import top
 
-# Create your tests here.
+
+class TopPageViewTest(TestCase):
+    def test_top_returns_200(self):
+        request = HttpRequest()
+        resnponse = top(request)
+        self.assertEqual(resnponse.status_code, 200)
+
+    def test_top_returns_expected_content(self):
+        request = HttpRequest()
+        resnponse = top(request)
+        self.assertEqual(resnponse.content, b"Hello World")
