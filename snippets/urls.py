ae036907d3443c82229a76857113277175192ef4
HirokiNakazawa <sp.hn_hc4@i.softbank.jp>
2021-08-15 12:16:28 +0900

    1.26

--- snippets/urls.py
+++ snippets/urls.py
@@ -4,7 +4,7 @@ from django.urls.resolvers import URLPattern
 from snippets import views
 
 urlpatterns = [
-    path("new/", views.snippets_new, name="snippet_new"),
+    path("new/", views.snippet_new, name="snippet_new"),
     path("<int:snippet_id>", views.snippet_detail, name="snippet_detail"),
-    path("<int:snippet_id>/edit/", views.snippets_edit, name="snippet_edit"),
+    path("<int:snippet_id>/edit/", views.snippet_edit, name="snippet_edit"),
 ]
